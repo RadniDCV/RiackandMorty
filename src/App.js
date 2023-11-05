7016369deabc8fb49bc86d3fda0bd23fbface6b7
RadniDCV <rjdcv1@gmail.com>
2023-11-05 09:37:34 -0400

    Primera carga

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import rickmorty from "./img/rick-morty.png";
+import "./App.css";
+import { useState } from "react";
+import Characters from "./components/Chraracters";
+
+function App() {
+  const [characters, setCharacters] = useState(null);
+
+  const repApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    const characterApi = await api.json();
+
+    setCharacters(characterApi.results);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title">Rick & Morty</h1>
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters} />
+        ) : (
+          <>
+            <img src={rickmorty} alt="Logo" className="img-home" />
+            <button onClick={repApi} className="btn-search">
+              Buscar personaje
+            </button>
+          </>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
